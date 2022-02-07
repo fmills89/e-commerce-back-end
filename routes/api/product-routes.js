1e8d31d01cf60b41576773c30296ad0a7528834d
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-02-06 18:26:16 -0600

    tag routes working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -111,11 +111,20 @@ router.post('/', (req, res) => {
 // update product
 router.put('/:id', (req, res) => {
   // update product data
-  Product.update(req.body, {
+  Product.update(
+    {
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
+      tag_Id: req.body.tagIds
+    },
+    {
     where: {
       id: req.params.id,
     },
-  })
+  }
+  )
     .then((product) => {
       // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
